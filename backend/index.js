06dbc86fa529f9b0710dde7ca35898391204b2bd
sainRaghuveer <raghuveersain987@gmail.com>
2023-04-29 14:56:52 +0530

    social app

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors=require("cors");
+const { connection } = require("./configs/db");
+
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World, welcome Social Media App....!!!!!!");
+});
+
+app.use("/user");
+
+app.use("/api")
+
+app.listen(process.env.port, async()=>{
+    try{
+        await connection;
+        console.log("connected with Social Media App DB")
+    }catch(error){
+        console.log(error);
+    }
+    console.log(`Server is running on port ${process.env.port}`);
+})
